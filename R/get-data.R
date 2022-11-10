36d71e290b60901b4dd7a6baae56988563a0c029
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-11-10 14:04:50 +0100

    Get data directly from Zotero

---
+++ R/get-data.R
@@ -0,0 +1,50 @@
+#' Import data from Zotero local database
+#' 
+#' @noRd
+
+get_data <- function(path = "~/Documents/Zotero", collection = "CESAB", 
+                     filename = "") {
+  
+  ## Check args ----
+  
+  if (is.null(collection)) {
+    stop("Argument 'collection' is required", call. = FALSE)
+  }
+  
+  if (!is.character(collection) || length(collection) != 1) {
+    stop("Argument 'collection' must be of length 1", call. = FALSE)
+  }
+  
+  if (!(collection %in% c("CESAB", "SDIV", "NCEAS"))) {
+    stop("Argument 'collection' must be one of 'CESAB', 'SDIV', 'NCEAS'",
+         call. = FALSE)
+  }
+  
+  
+  ## Get data ----
+  
+  refs <- zoteror::get_zotero_data(path)
+  
+  
+  ## Clean data ----
+  
+  refs <- refs[ , -c(1, 2)]
+  colnames(refs)[1] <- "library"
+  
+  
+  ## Select collection ----
+  
+  refs <- refs[refs$"collection" == collection, ]
+  rownames(refs) <- NULL
+  
+  
+  ## Export data ----
+  
+  if (filename == "") filename <- file.path("data", "raw-data", 
+                                            paste0(tolower(collection), 
+                                                   "-publications.txt"))
+  
+  write.table(refs, filename, sep = "\t", row.names = FALSE)
+  
+  invisible(refs)
+}
--- analyses/import-data.R
+++ analyses/import-data.R
@@ -1,3 +1,8 @@
+## Get CESAB data (from local Zotero) ----
+
+# get_data(path = "~/Documents/Zotero", collection = "CESAB")
+
+
 ## Import CESAB papers metadata ----
 
 cesab <- read.table(here::here("data", "raw-data", "cesab-publications.txt"), 
